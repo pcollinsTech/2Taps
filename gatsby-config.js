dd0b49454c44fd6c2f13662266dcc7099177a30f
Phil <phil@pcollins.tech>
2020-01-26 12:19:37 +0000

    favicon and title changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `2Taps Winebar`,
+    description: `Nestled in the heart of Belfast, boasting one of Belfast's largest outdoor dining experiences. 2Taps is the perfect place to meet, relax, share food and soak up the atmosphere.`,
+    author: `@pcollinsTech`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         start_url: `/`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
