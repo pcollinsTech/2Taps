00e300db3cbac6a6342e1313856b2b7cd048e5e7
Phil <phil@pcollins.tech>
2020-02-17 23:11:52 +0100

    prices food

--- src/components/Menus/foods/Lunch.js
+++ src/components/Menus/foods/Lunch.js
@@ -4,7 +4,7 @@ import MenuItem from "../MenuItem"
 const Lunch = ({ menu }) => {
   return (
     <Fragment>
-      <h3 className="my-3 mx-auto">12-5pm all lunch time course £11</h3>
+      <h3 className="my-3 mx-auto">12-5pm All Lunch Time Courses £11</h3>
       {menu.map(item => (
         <MenuItem
           title={item.title}
