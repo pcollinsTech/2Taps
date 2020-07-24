918936d2311d374b68f656b8edc7b351beaba84e
pcollinsTech <phil@pcollins.tech>
2020-07-24 12:18:14 +0100

    shorter

--- src/layout/Reservations.js
+++ src/layout/Reservations.js
@@ -21,7 +21,7 @@ const Reservations = ({ open }) => {
                 src="https://www.opentable.co.uk/r/2-taps-wine-bar-reservations-belfast?restref=161271&lang=en-GB&ot_source=Restaurant%20website"
                 frameborder="0"
                 width="400px"
-                height="800px"
+                height="600px"
                 title="OpenTable"
               ></iframe>
 
